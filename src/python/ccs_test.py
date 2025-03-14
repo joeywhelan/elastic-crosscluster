b4eb8ad614e172ca03c2bb6993b0fdf29ffe6a07
Joey Whelan <joey.whelan@gmail.com>
2025-03-14 10:50:05 -0600

    add comment regarding west es IP address

--- src/python/ccs_test.py
+++ src/python/ccs_test.py
@@ -1,6 +1,6 @@
 from elasticsearch import Elasticsearch
 
-WEST_IP = "172.18.0.5"
+WEST_IP = "172.18.0.4"  # change this to match the external IP assigned to westluster-es-http
 
 client = Elasticsearch(f"https://{WEST_IP}:9200", ca_certs="../../west/west-http-ca.crt", basic_auth=("elastic", "elastic"))
 resp = client.search(index=["west_ccs", "east_remote:east_ccs"], query={"range": {"release_date": {"gte": 1985}}})
